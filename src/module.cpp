b89b16298fabcf5e43b047615ef08c274824b31d
Marcin Cieślak <saper@saper.info>
2015-07-22 13:30:13 +0200

    Pass isolate to sub-objects

--- src/module.cpp
+++ src/module.cpp
@@ -11,12 +11,13 @@ using namespace v8;
 extern "C" void
 init(Handle<Object> target)
 {
-    HandleScope scope;
-    Gif::Initialize(target);
-    //FixedGifStack::Initialize(target);
-    DynamicGifStack::Initialize(target);
-    AnimatedGif::Initialize(target);
-    AsyncAnimatedGif::Initialize(target);
+    Isolate isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+    Gif::Initialize(isolate, target);
+    //FixedGifStack::Initialize(isolate, target);
+    DynamicGifStack::Initialize(isolate, target);
+    AnimatedGif::Initialize(isolate, target);
+    AsyncAnimatedGif::Initialize(isolate, target);
 }
 
 
