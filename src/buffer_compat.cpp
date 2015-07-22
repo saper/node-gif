638a5e94182b4e079332e6e7fabc8263c52f1dfa
Marcin Cieślak <saper@saper.info>
2015-07-22 12:35:08 +0200

    Temporary fix before removal

--- src/buffer_compat.cpp
+++ src/buffer_compat.cpp
@@ -4,24 +4,14 @@
 #include <v8.h>
 #include "buffer_compat.h"
 
-char *BufferData(node::Buffer *b) {
-  return node::Buffer::Data(b->handle_);
-}
-
-
-size_t BufferLength(node::Buffer *b) {
-  return node::Buffer::Length(b->handle_);
-}
-
-
 char *BufferData(v8::Local<v8::Object> buf_obj) {
-  v8::HandleScope scope;
+  v8::HandleScope scope(v8::Isolate::GetCurrent());
   return node::Buffer::Data(buf_obj);
 }
 
 
 size_t BufferLength(v8::Local<v8::Object> buf_obj) {
-  v8::HandleScope scope;
+  v8::HandleScope scope(v8::Isolate::GetCurrent());
   return node::Buffer::Length(buf_obj);
 }
 
